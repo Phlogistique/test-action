0ebc8bf4d77eae94120aecd3be2c042a59969c92
Noé Rubinstein <nrubinstein@scortex.io>
2020-03-30 19:24:50 +0200

    wip

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+(async function() {
+  const core = require("@actions/core");
+  const github = require("@actions/github");
 
-try {
-  const nameToGreet = core.getInput('who-to-greet');
-  const octokit = new github.GitHub(core.getInput("GITHUB_TOKEN"))
-  console.log(`Initialized connection to github`);
+  try {
+    const nameToGreet = core.getInput("who-to-greet");
+    const octokit = new github.GitHub(core.getInput("GITHUB_TOKEN"));
+    console.log(`Initialized connection to github`);
 
-  const pullRequests = await octokit.pulls.list(github.context.repo);
-  console.log(`Here are the pull requests! ${pullRequests}`);
+    const pullRequests = await octokit.pulls.list(github.context.repo);
+    console.log(`Here are the pull requests! ${pullRequests}`);
 
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
