15bcd45150deb7b35c7519f5cfa844a8d0c19d60
Noé Rubinstein <nrubinstein@scortex.io>
2020-05-23 11:45:45 +0200

    asdf

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const exec = require("@actions/exec");
-const filter = require("lodash.filter");
 const core = require("@actions/core");
 const github = require("@actions/github");
+const filter = require("lodash.filter");
+const minBy = require("lodash.minby");
 
 async function isAncestor(ancestor, descendent) {
   return (
@@ -10,27 +11,69 @@ async function isAncestor(ancestor, descendent) {
   );
 }
 
+async function exec_capture(cmd, args) {
+  let out = "";
+  let err = "";
+  const options = {
+    listeners: {
+      stdout: (data) => {
+        out += data.toString();
+      },
+      stderr: (data) => {
+        err += data.toString();
+      },
+    },
+  };
+  let retcode = await exec(cmd, args, options);
+  if (retcode != 0) {
+    throw `Invalid exit code ${retcode} for command ${cmd} ${args}:\n${err}`;
+  }
+
+  return out;
+}
+
+async function age(ancestor, descenent) {
+  return parseInt(
+    await exec_capture("git", ["rev-list", "--count", ancestor, descendent])
+  );
+}
+
 async function thisPrId() {
   return github.context.payload.pull_request.id;
 }
 
 async function findClosestAncestorPr(octokit, thisPullRequest) {
-    const pullRequests = await octokit.pulls.list(github.context.repo);
-    const other = filter(pullRequests.data, pr => pr.id != thisPullRequest);
+  const pullRequests = await octokit.pulls.list(github.context.repo);
+  const other = filter(pullRequests.data, (pr) => pr.id != thisPullRequest.id);
 
-    console.log(`pull requests: ${JSON.stringify(other, undefined, 2)}`)
+  console.log(`other pull requests: ${JSON.stringify(other, undefined, 2)}`);
+  console.log(`other pull requests: ${other.length}`);
 
+  if (other.length == 0) return null;
+
+  const ancestors = filter(other, (pr) =>
+    isAncestor(pr.head.sha, thisPullRequest.head.sha)
+  );
+
+  console.log(`ancestors: ${ancestors.length}`);
+
+  if (ancestors.length == 0) return null;
+
+  const mostRecentAncestor = minBy(ancestors, (pr) =>
+    age(pr.head.sha, thisPullRequest.head.sha)
+  );
+
+  return mostRecentAncestor;
 }
 
-(async function() {
-
+(async function () {
   try {
     const nameToGreet = core.getInput("who-to-greet");
     const octokit = new github.GitHub(core.getInput("GITHUB_TOKEN"));
     console.log(`Initialized connection to github`);
 
-    const thisPullRequest = github.context.payload.pull_request.id;
-    console.log(`Handling pull request ${thisPullRequest}`);
+    const thisPullRequest = github.context.payload.pull_request;
+    console.log(`Handling pull request ${thisPullRequest.id}`);
 
     await findClosestAncestorPr(octokit, thisPullRequest);
 
